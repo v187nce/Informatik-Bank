36d471b8e9ead939415e3b7dab8469e131a53a5e
v187nce <134378283+v187nce@users.noreply.github.com>
2023-09-04 13:29:36 +0000

    unterricht ende

--- Kunde.java
+++ Kunde.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Kunde{
 
     private String name; 
@@ -5,6 +7,9 @@ public class Kunde{
     private int kundennr; 
     private String anschrift; 
     private int PIN; 
+    private Konto konto1 = new Konto(12345, 120, null);
+    private Konto konto2 = new Konto(23456, 120, null);
+
   
     public Kunde(String name, String vorname, String anschrift, int kundennr, int PIN){
       this.name = name; 
@@ -24,5 +29,35 @@ public class Kunde{
     public void setAnschrift(String anschrift){this.anschrift = anschrift;}
     public void setKundennr(int kundennr){this.kundennr = kundennr;}
     public void setPIN(int PIN){this.PIN = PIN;} 
+
+    public void auszahlen(int betrag, int kontonr){
+        if (kontonr == konto1.getKontonr()){
+            if (konto1.getSaldo() >= betrag){
+                konto1.setSaldo(konto1.getSaldo() - betrag);
+                System.out.println("Sie haben nun " + betrag + " Euro auszahlen lassen." + "\n" + "Ihr neuer Kontostand beträgt nun: " + konto1.getSaldo());
+            }
+            
+        } else if (kontonr == konto2.getKontonr()){
+            if (konto2.getSaldo() >= betrag){
+                konto2.setSaldo(konto2.getSaldo() - betrag);
+                System.out.println("Sie haben nun " + betrag + " Euro auszahlen lassen." + "\n" + "Ihr neuer Kontostand beträgt nun: " + konto2.getSaldo());
+            }
+        }
+        
+    }
+
+    public void einzahlen(){
+
+    }
+
+    public void abheben(){
+
+    }
+
+    public void ueberweisen(){
+
+    }
+
+
     
   }
--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
-      Konto konto1 = new Girokonto(10201, 5550, 10000); 
-      System.out.println("Die Kontonummer von ihrem Konto entspricht: " + konto1.getKontonr());
+      Kunde kunde1 = new Kunde("Lakschuu", "Rabe", "Doktor", 123456, 123456);
+      kunde1.auszahlen(20, 23456);
     }
   }
